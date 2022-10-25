5b570d1e1049688514c895b9328eb20a7c1c8297
satbir1992 <satbirsingh.singh4@gmail.com>
2022-10-25 19:47:37 +0800

    Coding

---
+++ Drone.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus_Service_App
+{
+    internal class Drone
+    {
+        private string client_Name;
+        private string drone_Model;
+        private string service_Problem;
+        private double service_Cost;
+        private int service_Tag;
+
+        public Drone()
+        {
+
+        }
+        public Drone(string aclient_Name, string adrone_Model, string aservice_Problem, double aservice_Cost, int aservice_Tag)
+        {
+            client_Name = aclient_Name;
+            drone_Model = adrone_Model;
+            service_Problem = aservice_Problem;
+            service_Cost = aservice_Cost;
+            service_Tag = aservice_Tag;
+        }
+        public void setName(string aClientName)
+        {
+            client_Name=aClientName;
+        }
+        public string getName()
+        {
+            return client_Name;
+        }
+        public void setModel(string aDroneModel)
+        {
+            drone_Model = aDroneModel;
+        }
+        public string getModel()
+        {
+            return drone_Model;
+        }
+        public void setProblem(string aServiceProblem)
+        {
+            service_Problem = aServiceProblem;
+        }
+        public string getProblem()
+        {
+            return service_Problem;
+        }
+        public void setCost(double aServiceCost)
+        {
+            service_Cost=aServiceCost;
+        }
+        public double getCost()
+        {
+            return (service_Cost);
+        }
+        public void setTag(int aServiceTag)
+        {
+            service_Tag=aServiceTag;
+        }
+        public int getTag()
+        {
+            return service_Tag;
+        }
+        public string DisplayNameCost()
+        {
+            return client_Name + "     "+service_Problem;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using Syncfusion.Windows.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
+
 namespace Icarus_Service_App
 {
     /// <summary>
@@ -25,5 +28,49 @@ namespace Icarus_Service_App
         {
             InitializeComponent();
         }
+        List<Drone> finishedList = new List<Drone>();
+        Queue<Drone> expressQueue = new Queue<Drone>();
+        Queue<Drone> regularQueue = new Queue<Drone>();
+        
+        
+        
+       
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Drone addDrone = new Drone();
+            addDrone.setName(TextBoxName.Text);
+            addDrone.setCost(Convert.ToDouble(TextBoxCost.Text));
+            addDrone.setProblem(TextBoxProblem.Text);
+            addDrone.setModel(TextBoxModel.Text);
+            addDrone.setTag(100);
+            expressQueue.Enqueue(addDrone);
+            DisplayQueue();
+         }
+        public void DisplayQueue()
+        {
+            ListViewExpress.Items.Clear();
+            foreach(Drone information in expressQueue) {
+
+                var row = new { Client_Name=information.getName(), Drone_Model=information.getModel(), Service_Problem=information.getProblem(),
+                    Service_Cost = information.getCost(),Service_Tag = information.getTag()};
+                ListViewExpress.Items.Add(row); 
+            }
+        }
+        private void setRadioButton(int serviceType)
+        {
+            foreach(RadioButton rb in ServiceOption.Children.OfType<RadioButton>())
+            {
+                if(rb.IsChecked == true)
+                {
+
+                }
+            }
+        }
     }
+   
 }
